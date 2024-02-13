cc98ce0a96e8b2916ebb6bb2ba86b2dc0f7e654b
StockingAnarcy <stockinanarcy@gmail.com>
2024-02-13 13:25:03 +0300

    Добавьте файлы проекта.

---
+++ Controllers/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace Calculator.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        [Route("add")]
+        public ActionResult<int> Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [HttpGet("subtract")]
+        public ActionResult<int> Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        
+        [HttpGet("multiply")]
+        public ActionResult<int> Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        [HttpGet("divide")]
+        public ActionResult<double> Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                return BadRequest("Division by zero is not allowed.");
+            }
+            return (double)a / b;
+        }
+    }
+}
+
